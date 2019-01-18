505c7a6305d5fe681683123804b074baf735950a
zhangguozhong <guozhong.zhang@mobilemd.cn>
2019-01-18 16:40:02 +0800

    init

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
 import configHeader from "../configHeader";
 import serverEnv from "../serverEnv";
 import httpClient from "../index";
+import serverApi from "../serverApi/serverApi";
 
 export {
     configHeader,
     serverEnv,
+    serverApi,
     httpClient
 }
